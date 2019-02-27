6aad9bb698a1ea248dec563efa756d552f39ee53
Sniadek <wojokarolg9@gmail.com>
2019-02-28 00:49:21 +0100

    add actions to save tokens to local storage

--- src/Actions/auth.ts
+++ src/Actions/auth.ts
@@ -1,6 +1,18 @@
 import { TokenStatus, TokenAction, Action } from "./index";
+import * as store from "store";
+
+
+export function getTokenFromLocalStorage(): TokenAction {
+    const token = store.get("token", "");
+    return {
+        token,
+        type: Action.SAVE_TOKEN
+    }
+}
 
 export function saveToken(token: string): TokenAction {
+    // save token in local storage
+    store.set("token", token);
     return {
         token,
         type: Action.SAVE_TOKEN
@@ -10,7 +22,7 @@ export function saveToken(token: string): TokenAction {
 
 export function deleteToken(): TokenAction {
     return {
-        token : "",
+        token: "",
         type: Action.DELETE_TOKEN
     }
 }
